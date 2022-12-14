9af419a243fbc6cae6ac361881a863098529af59
Yuan Cao <yuan876@revature.net>
2022-12-14 04:54:55 -0500

    Add CrossOrigin

--- src/main/java/org/example/sba/controllers/HelloWorldController.java
+++ src/main/java/org/example/sba/controllers/HelloWorldController.java
@@ -1,10 +1,8 @@
 package org.example.sba.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class HelloWorldController {
     @RequestMapping("/")
